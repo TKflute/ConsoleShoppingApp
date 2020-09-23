e95a6ced65f8808084a824f5509db3f6e146a6fe
Tara Kelly <kellyt2@students.westerntc.edu>
2020-09-23 11:19:52 -0500

    added OrderItem class inside Order class

--- CoreJavaStandAloneShoppingApp/src/com/cognixia/jumplus/model/Order.java
+++ CoreJavaStandAloneShoppingApp/src/com/cognixia/jumplus/model/Order.java
@@ -1,8 +1,19 @@
 package com.cognixia.jumplus.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
 
-	// maybe use an inner class called OrderItems like I did for .NET project (CartLines)
+	// maybe use an inner class called OrderItem like I did for .NET project (CartLine)
+	// will maybe choose a diff collection based on how I need to access the items
+	private List<OrderItem> orderItems = new ArrayList<OrderItem>();
 	
-	
+	public class OrderItem{
+		
+		private long id;
+		private Item item;
+		private int qty;
+		
+	}
 }
